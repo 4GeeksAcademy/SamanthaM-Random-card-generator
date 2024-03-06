8359e228c4301df7922b38306f478b27c1051a2a
Samantha McCabe <156986930+SamanthaMcCabe2104@users.noreply.github.com>
2024-03-06 22:59:06 +0000

    fixed suit generation

--- src/app.js
+++ src/app.js
@@ -6,18 +6,44 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = () => {
+  randomize();
+};
+
+function randomize() {
+  let suits = ["diamond", "spade", "heart", "club"];
+  for (const suit of suits) {
+    document.querySelector(".card").classList.remove(suit);
+  }
+
   document.querySelector(".card").classList.add(generateRandomSuit());
   document.querySelector(".card").innerHTML = generateRandomNumber();
-};
+}
 
-let generateRandomNumber = () => {
-  let numbers = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+function generateRandomNumber() {
+  let numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+
   let indexNumbers = Math.floor(Math.random() * numbers.length);
-  return numbers[indexNumbers];
-};
 
-let generateRandomSuit = () => {
+  return numbers[indexNumbers];
+}
+
+function generateRandomSuit() {
   let suit = ["diamond", "spade", "heart", "club"];
   let indexSuit = Math.floor(Math.random() * suit.length);
+
   return suit[indexSuit];
-};
+}
